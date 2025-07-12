cd8b34b0a03f69160d3e189f89f7809c243a0968
kbandison <kbandison@gmail.com>
2025-07-12 04:21:42 -0400

    fixed contact form and package carousel, added projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -222,6 +223,7 @@ export default function RootLayout({
       >
         <Navigation />
         <Analytics />
+        <SpeedInsights />
         {children}
         <Footer />
       </body>
